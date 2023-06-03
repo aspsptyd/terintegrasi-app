1afa747cb2ea00188fbf4185704c33505450a36c
akunduwa <septiadi.imail@gmail.com>
2023-06-04 05:59:51 +0700

    Penyesuaian inputan untuk menambah user baru

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'roles',
+        'phone_number',
+        'phone_number_verified_at'
     ];
 
     /**
