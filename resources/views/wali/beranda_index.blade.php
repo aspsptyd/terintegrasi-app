84a8da7bfc7a8cb435aa6c02daa2f7773ff75ffc
aspsptyd <devadiroot@gmail.com>
2023-06-12 13:42:27 +0700

    Sesuaikan layout container untuk layout role wali

--- resources/views/wali/beranda_index.blade.php
+++ resources/views/wali/beranda_index.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app_sneat')
 
 @section('content')
-<div class="col-md-12">
-    <div class="card">
-        <div class="card-header">Hi, {{ auth()->user()->name }} your as <span class="fw-bold">{{ auth()->user()->roles === 'wali' ? 'Wali Murid' : 'Undefined!' }}</span></div>
-
-        <div class="card-body">
-            Welcome in <span class="fw-bold">Admin Template {{ env('TEMPLATE_ADMIN_VERSION') }}</span>, And you are logged in now!
+<div class="container-fluid flex-grow-1 container-p-y">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">Hi, {{ auth()->user()->name }} your as <span class="fw-bold">{{ auth()->user()->roles === 'wali' ? 'Wali Murid' : 'Undefined!' }}</span></div>
+    
+            <div class="card-body">
+                Welcome in <span class="fw-bold">Admin Template {{ env('TEMPLATE_ADMIN_VERSION') }}</span>, And you are logged in now!
+            </div>
         </div>
     </div>
 </div>
+
 @endsection
